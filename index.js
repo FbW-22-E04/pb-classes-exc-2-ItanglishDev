7d725f100930603123eb6b4732bee2be51519d04
chris <chrishethertonweb@gmail.com>
2022-08-25 14:15:51 +0200

    ex 2 done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 class Employee {
 
   constructor(id, firstName, lastName, taxId, salary) {
+
     this.id = id
     this.firstName = firstName
     this.lastName = lastName
@@ -14,3 +15,34 @@ class Employee {
     return this.Employee
   }
 }
+
+// Ex 2
+
+class Manager extends Employee {
+
+  constructor(id, firstName, lastName, taxId, salary) {
+
+
+    super(id, firstName, lastName, taxId, salary)
+
+    this.managedEmployees = []
+  }
+
+  addManagedEmployee(id, firstName, lastName, taxId, salary) {
+    this.managedEmployees.push({ id, firstName, lastName, taxId, salary })
+  }
+
+  removeManagedEmployee(employee) {
+    for (employee of managedEmployees) idx = ((managedEmployees.indexOf(employee)))
+    this.managedEmployees.slice(employee[idx], 1)
+  }
+}
+
+const atAndT = new Manager()
+const atAndT2 = new Manager()
+
+atAndT2.addManagedEmployee(2, 'chris', 'het', 0123, 5000)
+atAndT.addManagedEmployee(2, 'chris', 'het', 0123, 5000)
+atAndT.addManagedEmployee(45, 'Kirk', 'mcs', 536235, 37613)
+console.log("atnt", atAndT.managedEmployees);
+console.log("atnt2", atAndT2.managedEmployees);
