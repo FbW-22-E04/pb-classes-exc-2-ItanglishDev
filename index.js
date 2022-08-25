28f7b4ca9eed738d7089685912af07eeb234508b
chris <chrishethertonweb@gmail.com>
2022-08-25 13:16:53 +0200

    ex 1 done

---
+++ index.js
@@ -0,0 +1,16 @@
+// EX 1
+
+class Employee {
+
+  constructor(id, firstName, lastName, taxId, salary) {
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.taxId = taxId
+    this.salary = salary
+  }
+
+  generatePaySlip() {
+    return this.Employee
+  }
+}
